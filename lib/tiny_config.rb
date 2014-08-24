17262997967b99a7a65854132c9ff147fb113541
sho-ogihara <sho-ogihara@gree.net>
2014-08-24 11:48:14 +0900

    implement README interface

--- lib/tiny_config.rb
+++ lib/tiny_config.rb
@@ -1,24 +1,40 @@
-require "tiny_config/version"
+#require "tiny_config/version"
+require 'singleton'
 
 class TinyConfig
   def self.register(namespace, config_keys)
-    config_keys.each do |config_key|
-      const_set namespace.capitalize, Class.new {
-#        class << self
-#          attr_writer config_key
-#        end
+    const_set namespace.capitalize, Class.new {
+      include ::Singleton
 
-        define_singleton_method config_key do
+      config_keys.each do |config_key|
+        attr_writer config_key
+
+        define_method config_key do
+          v = instance_variable_get("@#{config_key}")
+          v.nil? ? raise(TinyConfig::ConfigKeyNillError, message) : v
         end
-      }
-    end
+      end
+
+      def message
+        service_name = self.class.to_s.split('::').last.downcase
+        required_key = caller()[1].match(/`.*'?/).to_s.delete('`\'')
+        "#{service_name} #{required_key} is requried."
+      end
+    }
   end
 
   def self.method_missing namespace
-    const_get(namespace.to_s.capitalize)
+    const_get(namespace.capitalize).instance
   end
+
+  class ConfigKeyNillError < StandardError; end
 end
 
-TinyConfig.register(:twitter, [:access_key])
-TinyConfig.twitter.access_key = 'hi'
-TinyConfig.twitter.access_key
+#TinyConfig.register(:twitter, [:access_key, :access_sec])
+#TinyConfig.twitter.access_key = 'hi'
+#puts TinyConfig.twitter.access_key
+#begin
+#  TinyConfig.twitter.access_sec
+#rescue TinyConfig::ConfigKeyNillError
+#  puts 'hi'
+#end
